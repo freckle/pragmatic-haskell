53380d1db68f2824726baaa850dc89b2873581f4
Alexandr Kurilin <alex@kurilin.net>
2016-04-20 12:01:22 -0700

    Add FooParser (WIP)

---
+++ src/FooParser.hs
@@ -0,0 +1,7 @@
+module FooParser where
+
+import Data.Text
+import Text.Parsec.Text (Parser)
+
+parse :: Text -> Parser Text
+parse = undefined
---
+++ test/FooParserSpec.hs
@@ -0,0 +1,14 @@
+module FooParserSpec (main, spec) where
+
+import Test.Hspec
+import FooParser (parse)
+
+-- main :: IO ()
+-- main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "do stuff" $
+    it "tests a thing" $ do
+      1 `shouldBe` 2
+      parse "foo" `shouldBe` "foo"
