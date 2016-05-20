2c9582a08368239bbf91908de947558f6d497080
Alexandr Kurilin <alex@kurilin.net>
2016-05-19 20:13:54 -0700

    Add basic persistent & esqueleto examples

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,22 @@ import Database.Persist.Sqlite
 import Text.Megaparsec
 import FooParser (parseSections, Section(..))
 import Data.List (unwords)
+import qualified Database.Esqueleto as E
 
 main :: IO ()
 main = do
   fileContents <- readFile "specs.foo"
 
-  -- could be pure but we're using print so we'll get an IO back
+  -- parse specs.foo
   let sections = case runParser parseSections "specs.foo" fileContents of
         Left err        -> error $ "could not parse .foo file with error: " ++ show err
         Right sections' -> sections'
 
+  -- run everything inside single connection/txn against an in-memory
+  -- instance of SQLite
   runSqlite ":memory:" $ do
+
+    -- run a safe migration
     runMigration migrateAll
 
     -- prepare users, insert users, get entities
@@ -34,9 +39,49 @@ main = do
 
     parKeys <- insertMany paragraphs
 
-    liftIO $ print "all done"
+    -- let's fetch all users (note GHC type hint here)
+    users' <- selectList ([] :: [Filter User]) []
+    liftIO $ putStrLn ""
+    liftIO $ putStrLn "Users:"
+    liftIO $ print users'
 
-    -- TODO: talk about TH
-    -- TODO: talk about Entities in persistent
-    -- TODO: something interesting in esqueleto, preferably composable
-    -- TODO: tests
+    -- let's fetch all users with filter, (questionably useful) limit and sorting
+    users'' <- selectList ([UserName ==. "alex"]) [LimitTo 1, Asc UserName]
+    liftIO $ putStrLn ""
+    liftIO $ putStrLn "Users:"
+    liftIO $ print users''
+
+    allParags <- selectList ([] :: [Filter Paragraph]) []
+    liftIO $ putStrLn ""
+    liftIO $ putStrLn "All Paragraphs:"
+    liftIO $ print allParags
+
+    -- uh oh.. how do I join? esqueleto to the rescue
+    alexParags <- E.select $
+                  E.from $ \(users `E.LeftOuterJoin` paragraphs) -> do
+                  E.on ((users E.^. UserId) E.==. (paragraphs E.^.ParagraphUserId))
+                  E.where_ ((users E.^. UserName) E.==. (E.val "alex"))
+                  return paragraphs
+
+    liftIO $ putStrLn ""
+    liftIO $ putStrLn "Alex's Paragraphs:"
+    liftIO $ print alexParags
+
+    -- we can compose! Define fn giving us alex where clause
+    let whereClause table = E.where_ ((table E.^. UserName) E.==. (E.val "alex"))
+
+    alexFirstParag <- E.select $
+                      E.from $ \(users `E.LeftOuterJoin` paragraphs) -> do
+                      E.on ((users E.^. UserId) E.==. (paragraphs E.^.ParagraphUserId))
+                      (whereClause users) -- can reuse this!
+                      E.limit 1
+                      return paragraphs
+
+    liftIO $ putStrLn ""
+    liftIO $ putStrLn "Alex's First Paragraph:"
+    liftIO $ print alexFirstParag
+
+    liftIO $ putStrLn ""
+    liftIO $ print "--- all done ---"
+
+    -- TODO: tests against DB perhaps?
--- src/Entities.hs
+++ src/Entities.hs
@@ -5,6 +5,7 @@ import           Database.Persist.TH
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 User sql=users
     name String
+    UniqueName name
     deriving Show Eq
 Paragraph sql=paragraphs
     content String
